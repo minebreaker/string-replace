fd4da7116a1332ba20f32642ba7878b7c2360051
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2019-03-30 21:37:45 +0900

    added javadoc

--- javac-string-replace-plugin/src/main/java/rip/deadcode/javac/stringreplace/Replace.java
+++ javac-string-replace-plugin/src/main/java/rip/deadcode/javac/stringreplace/Replace.java
@@ -3,6 +3,10 @@ package rip.deadcode.javac.stringreplace;
 import java.lang.annotation.*;
 
 
+/**
+ * The value of the field annotated by this annotation will be replaced
+ * by the annotation argument passed to javac.
+ */
 @Retention( RetentionPolicy.CLASS )
 @Target( ElementType.FIELD )
 @Documented
